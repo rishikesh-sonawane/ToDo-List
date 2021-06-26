985ff967167c2504659f4ef822c7c83bf9dd448d
Rishikesh_Sonawane <rishikeshsonawane1465@gmail.com>
2021-06-26 14:15:19 +0530

    This is my first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Hello! from Rishikesh Sonawane
+            <p>And this is my First Project using ReactJS. I Hope you Like It :)</p>
+        </div>
+    )
+}
